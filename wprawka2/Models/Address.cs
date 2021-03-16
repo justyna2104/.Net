14a0ce682e9645a1495c2f9cda89fbaf63d4e5c3
justyna2104 <justynaharmuszkiewicz@gmail.com>
2021-03-16 02:06:13 +0100

    Add project files.

---
+++ wprawka2/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wprawka2.Models
+{
+    public class Address
+    {
+
+        [Display(Name = "Your street")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest obowiazkowe")]
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+
+    }
+}
---
+++ wprawka2/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using wprawka2.Models;
+
+namespace wprawka2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                 Name = "User";
+            }
+           
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
